4bb398022ce80dc40eef616c220e0b033ed5a464
Jodie Cristobal <jod_bod02@yahoo.com>
2022-05-16 18:47:13 +0800

    FIX: Remove user enrollment in routes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const errorHandler = require('../middlewares/error-handler');
 const app = express();
 
 const health = require('./health');
-const userEnrollment = require('./user-enrollment');
 const artist = require('./artist')
 
 const router = express.Router();
@@ -21,7 +20,6 @@ app.use(errorHandler.handleError);
  * Add routes
  */
 router.use('/health', health);
-router.use('/user',userEnrollment);
 router.use('/artist',artist);
 
 module.exports = router;
